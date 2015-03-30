a6eece647bf57638de48d68dfc4b83eaf97837a7
Caleb Anderson <robotrising@gmail.com>
2015-03-29 23:20:02 -0600

    final checkpoint session 3

--- Sessions/03 - Further Perlin Noise with Cube/js/perlin.js
+++ Sessions/03 - Further Perlin Noise with Cube/js/perlin.js
@@ -1,6 +1,6 @@
 var rgb = function(r, g, b) {
   return 'rgb(' + r + ','+ g + ','+ b + ')';
-}
+};
 
 var canvas = {};
 var ctx = {};
@@ -12,7 +12,7 @@ var pixelSizeX = 10;
 var pixelSizeY = 10;
 
 var perlinCycle          = 0;
-var perlinCycleIncrement = 0.025;
+var perlinCycleIncrement = 0.0125;
 
 var pixels    = [];
 var numPixels = 256;
@@ -39,30 +39,39 @@ var init = function() {
 
   noise.seed(Math.random());
 
-  while (ribbons.length < numRibbons) {
-    var color = getColor();
-    var seedX = Math.random() * 2;
-    var seedY = Math.random() * 2;
-    var seed = new Vector(seedX, seedY);
-    var ribbon = new Ribbon(w / 2, h / 2, color, seed);
-    ribbons.push(ribbon);
-  }
+  var palette = [
+    [255, 65,  202],
+    [95,  0,   127],
+    [35,  53,  190],
+    [195, 33,  165],
+    [42,  245, 255],
+  ];
+  palettes.push(palette);
 
-  var palette = [];
-  palette.push([255,65, 202]);
-  palette.push([95, 0,  127]);
-  palette.push([35, 53, 190]);
-  palette.push([195,33, 165]);
-  palette.push([42, 245,255]);
+  palette = [
+    [238, 200, 43],
+    [214, 150, 28],
+    [169, 98,  50],
+    [154, 37,  17],
+    [86,  0,   0],
+  ];
+  palettes.push(palette);
 
+  palette = [
+    [255, 255, 255],
+    [181, 216, 246],
+    [84,  173, 234],
+    [74,  159, 219],
+    [59,  129, 178],
+  ];
   palettes.push(palette);
 
   requestAnimationFrame(tick);
-}
+};
 
 var tick = function() {
   var now = Date.now();
-  if (lastUpdate == 0) {
+  if (lastUpdate === 0) {
     lastUpdate = now;
   }
   var dt = (now - lastUpdate) / 1000;
@@ -73,110 +82,111 @@ var tick = function() {
 
   fillCanvas(backgroundColor);
 
-  //perlinRibbon(dt, w, h);
   perlinPlot(dt, w, h);
-  //perlinRibbons(dt, w, h);
 
   perlinCycle += perlinCycleIncrement;
 
   requestAnimationFrame(tick);
-}
-
-var perlinRibbons = function(dt, w, h) {
-
-  for (var i = 0; i < numRibbons; ++i) {
-    var ribbon = ribbons[i];
-    var seed = ribbon.getSeed();
-
-    var position = noise.perlin3(seed.x, seed.y, perlinCycle);
-    position *= ribbonYDeviation;
-    ribbon.addPoint(w * .8, h / 2 + position);
-
-    ribbon.update(dt);
-  }
-
-  for (var i = 0; i < numRibbons; ++i) {
-    var ribbon = ribbons[i];
-
-    ribbon.draw(ctx, w, h);
-  }
-}
-
-var perlinRibbon = function(dt, w, h) {
-  fillCanvas(backgroundColor);
-
-  var pixelIndex = noise.perlin3(0, 0, perlinCycle);
-  pixelIndex = Math.floor(scaleNoise(pixelIndex) * 255);
-
-  var position = noise.perlin3(0.5, 0.5, perlinCycle);
-  position *= 200;
-
-  var w = canvas.width;
-  var h = canvas.height;
-
-  var pixel = pixels[pixelIndex];
-  ctx.putImageData(pixel, w / 2, h / 2 + position);
-
-  //shiftCanvas(-5);
-}
+};
 
 var getColorFromPalette = function(noiseValue) {
-  var palette = palettes[0];
+  var palette = palettes[1];
 
-  var c = assignColor(noiseValue, 3, 2, 3);
-  var palColor = palette[c];
-  var newColor =
-    [Math.round(palColor[0] * noiseValue / 255),
-     Math.round(palColor[1] * noiseValue / 255),
-     Math.round(palColor[2] * noiseValue / 255)];
- // return rgb(palColor[0], palColor[1], palColor[2]);
+  var c              = assignColor(noiseValue, [4, 3, 2, 3, 4]);
+  var palColorResult = palette[c[0]];
+  var palColor       = palColorResult;
+
+  // Example:
+  // noiseValue = 128
+  // weightBegin = 7
+  // weightEnd = 9
+  // weightSum = 16
+  // 2/16 of total range used... that is, only 111 to 143 can be in this range.
+  // We want to scale this to 255
+  // so, 111 => 0, 143 => 255
+  // Range = 32
+  //
+  // Test case: 128:
+  //  128 - 111 = (17 / 32) * 255 = 135
+
+  var weightBegin = c[1]; // 7
+  var weightEnd   = c[2]; // 9
+  var weightTotal = c[3]; // 16
+
+  var rangeBegin = weightBegin / weightTotal * 255; // 111
+  var rangeEnd   = weightEnd / weightTotal * 255;   // 143
+  var range      = rangeEnd - rangeBegin;           // 32
+
+  var rangeMultiplier = (noiseValue - rangeBegin) / range;
+  rangeMultiplier = Math.max(rangeMultiplier, 0.4);
+
+  var r = palColor[0] * rangeMultiplier;
+  var g = palColor[1] * rangeMultiplier;
+  var b = palColor[2] * rangeMultiplier;
+
+  var newColor = [
+    Math.round(r),
+    Math.round(g),
+    Math.round(b)
+  ];
   return rgb(newColor[0], newColor[1], newColor[2]);
-}
+};
 
-var assignColor = function(noiseValue, lowWeight, midWeight, highWeight) {
-  var totalWeight = lowWeight + midWeight + highWeight;
+// Color Talk
+// 0-25    [middle = 12]    Red
+// 26-200  [middle = 113]   Green
+// 201-255 [middle = 228]   Blue   |---|----------------|---+-|
+//                                 |--------------------+---+-|
+// 113 => [0,255] = 127
+//
+//
+// |---|---+------------|-----|
+// 25% between Green bottom & Green top.
+// Full intensity Green = 50%
+// 25% Red + 75% Green
+//
+// |---|+---------------|-----|
+// 50% Red + 50% Green
 
-  if (noiseValue < 255 * lowWeight / totalWeight)
-  {
-    return 0;
-  }
-  else if (noiseValue < 255 * (lowWeight + midWeight) / totalWeight)
-  {
-    return 1;
-  }
-  else
-  {
-    return 2;
+
+var assignColor = function(noiseValue, weights) {
+  var totalWeight = 0;
+  for (var n = 0; n < weights.length; ++n) {
+    totalWeight += weights[n];
   }
 
-}
+  var weightSum = 0;
+  for (var i = 0; i < weights.length; ++i) {
+    var weightBegin = weightSum;
+    var weightEnd   = weightBegin + weights[i];
+    weightSum       = weightEnd;
+    if (noiseValue < 255 * weightSum / totalWeight)
+    {
+      return [i, weightBegin, weightEnd, totalWeight];
+    }
+  }
+};
 
 var perlinPlot = function(dt, w, h) {
+  var pixelGap = 0;
   for (var x = 0; x < pixelsX; ++x) {
     for (var y = 0; y < pixelsY; ++y) {
       var noiseValue = noise.perlin3(x * 0.1, y * 0.1, perlinCycle);
       noiseValue = Math.floor(scaleNoise(noiseValue) * 255);
 
       ctx.fillStyle = getColorFromPalette(noiseValue, 3, 2, 3);
-      ctx.fillRect(x * (pixelSizeX + 1), y * (pixelSizeY + 1), pixelSizeX, pixelSizeY);
+      ctx.fillRect(x * (pixelSizeX + pixelGap), y * (pixelSizeY + pixelGap), pixelSizeX, pixelSizeY);
     }
   }
-}
+};
 
 var scaleNoise = function(val) {
   return (val + 1) / 2;
-}
+};
 
 var resize = function() {
-  var w = canvas.width = window.innerWidth / 2;
+  var w = canvas.width  = window.innerWidth / 2;
   var h = canvas.height = window.innerHeight / 2;
-
-  for (var i = 0; i < numPixels; ++i) {
-    ctx.fillStyle = rgb(i, i, i);
-    ctx.fillRect(0, 0, pixelSizeX, pixelSizeY);
-    var pixel = ctx.getImageData(0, 0, pixelSizeX, pixelSizeY);
-    pixels.push(pixel);
-  }
 
   fillCanvas(backgroundColor);
 
@@ -185,18 +195,9 @@ var resize = function() {
   ret.h = h;
 
   return ret;
-}
+};
 
 var fillCanvas = function(color) {
   ctx.fillStyle = color;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
-
-var shiftCanvas = function (dirX) {
-  var image = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  ctx.putImageData(image, dirX, 0);
-}
-
-var getColor = function() {
-  return '#'+Math.floor(Math.random()*16777215).toString(16);
-}
+};
